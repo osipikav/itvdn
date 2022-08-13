8bc423d86bd1d4e9caeb45eeeb68f3b4454074ab
osipik.a <osipik.av@yandex.by>
2022-08-13 16:42:40 +0300

    feat: create personal card

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hi");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let lastFilm = prompt("Один из последних просмотренных фильмов?");
+let lastFilmScore = prompt("На сколько оцените его?");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    lastFilm: lastFilmScore,
+  },
+  actors: {},
+  genres: [],
+  privat: false,
+};
+console.log(personalMovieDB);
